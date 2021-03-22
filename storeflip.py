ca052d3cb6ff72997f1660a7d7f16feeaff061e8
Robert Miller <robert.miller@loblaw.ca>
2021-03-22 14:49:03 -0400

    Location, Client Communities, and Network services completed

--- storeflip.py
+++ storeflip.py
@@ -47,6 +47,7 @@ else:
     print
     "Site name change failed", response.status_code
 
+
 # Client Communities Flip
 
 response = requests.get('https://10.104.8.111:8443/ng1api/ncm/clientcommunities/{0}'.format(old_site_name), headers=headers,
@@ -69,6 +70,36 @@ old_name = response.text
 new_name = old_name.replace(old_site_name, new_site_name, 2)
 
 response = requests.put('https://10.104.8.111:8443/ng1api/ncm/clientcommunities/{0}'.format(old_site_name), headers=headers,
+                        cookies=cookies, data=new_name, verify=False)
+if response.status_code == 200:
+    print
+    "Site", old_site_name, "changed to", new_site_name
+else:
+    print
+    "Site name change failed", response.status_code
+
+# Network Services Flip
+
+response = requests.get('https://10.104.8.111:8443/ng1api/ncm/services/{0}'.format(old_site_name), headers=headers,
+                        cookies=cookies, data=new_name, verify=False)
+
+if response.status_code == 404:
+    print()
+    "Site", old_site_name, "Not Found", response.status_code
+    sys.exit(response.status_code)
+elif response.status_code == 200:
+    print
+    "Found Site:", old_site_name
+else:
+    print
+    "something else went wrong ", response.status_code
+    sys.exit(response.status_code)
+
+old_name = response.text
+
+new_name = old_name.replace(old_site_name, new_site_name, 2)
+
+response = requests.put('https://10.104.8.111:8443/ng1api/ncm/services/{0}'.format(old_site_name), headers=headers,
                         cookies=cookies, data=new_name, verify=False)
 if response.status_code == 200:
     print
